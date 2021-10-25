4111ef6459b4e084ef8b82854300efb7af222fc3
Lucas Tomas Micheli <LTMicheli@dmenargas.gov.ar>
2021-10-25 09:53:38 -0300

    se cambio el arraylist por un hashmap

--- LenguajeEsencial/src/compilador_esencial/ListaVariables.java
+++ LenguajeEsencial/src/compilador_esencial/ListaVariables.java
@@ -1,35 +1,30 @@
 package compilador_esencial;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ListaVariables {
-	
-	private static ArrayList<Variable> variables = new ArrayList<>();
-	
-	public static void actualizarVariable(Variable var) {
-		for(Variable v : variables) {
-			if(v.getNombre().equals(var.getNombre())) v.setValor(var.getValor());
-		}
-	}
-	
-	public static Variable obtenerVariable(String nombreVariable) {
-		for(Variable v : variables) {
-			if(v.getNombre().equals(nombreVariable)) return v;
-		}
-		return null;
-	}
+
+	private static HashMap<String, Integer> variables = new HashMap<>();
 	
 	public static Variable crearVariable(String nombreVariable) {
 		Variable var = new Variable(nombreVariable);
-		ListaVariables.variables.add(var);
+		variables.put(var.getNombre(),var.getValor());
 		return var;
 	}
-	
+
 	public static Variable obtenerOCrearVariable(String nombreVariable) {
 		Variable v = obtenerVariable(nombreVariable);
 		if(v != null) return v;
 		return crearVariable(nombreVariable);
 	}
 
+	public static Variable obtenerVariable(String nombreVariable) {
+		if (variables.get(nombreVariable) == null) return null;
+		return new Variable(nombreVariable, variables.get(nombreVariable));
+	}
+
+	public static void actualizarVariable(Variable var) {
+		variables.put(var.getNombre(), var.getValor());
+	}
 
 }
